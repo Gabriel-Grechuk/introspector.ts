61c20e15ed7c4750bd869015c5e46a130d8e6d04
Gabe <gabrielgrechuk@outlook.com>
2025-04-29 01:30:47 -0300

    #4 - Add all knex deps

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,32 @@ import { genFileText, snakeToPascal, writeFile } from "./tools";
 import * as logging from "./logging";
 
 async function main() {
-  try {
-    const args = parseCliArgs();
-    const instances = instancePostgresDatabases({
-      databases: args.databases,
-      ssl: args.ssl,
-    });
-    const schemas = await getPostgresDatabasesSchema({
-      databases: instances,
-      tablesToIgnore: args.exclude_tables,
-    });
+  const args = parseCliArgs();
+  const instances = instancePostgresDatabases({
+    databases: args.databases,
+    ssl: args.ssl,
+  });
+  const schemas = await getPostgresDatabasesSchema({
+    databases: instances,
+    tablesToIgnore: args.exclude_tables,
+  });
 
-    const writeInterfacesPromises = Object.keys(schemas).map((field) => {
-      return writeFile(
-        `${args.outdir ?? "."}/${snakeToPascal(field)}.interfaces.ts`,
-        genFileText(schemas[field]!),
-      );
-    });
+  const writeInterfacesPromises = Object.keys(schemas).map((field) => {
+    return writeFile(
+      `${args.outdir ?? "."}/${snakeToPascal(field)}.interfaces.ts`,
+      genFileText(schemas[field]!),
+    );
+  });
 
-    await Promise.all(writeInterfacesPromises);
-  } catch (error) {
-    console.log("Error:", error);
-  }
+  await Promise.all(writeInterfacesPromises);
 }
 
-await main().then(() => {
-  logging.infoLog("All done! Exiting...");
-  process.exit(0);
-});
+await main()
+  .then(() => {
+    logging.infoLog("All done! Exiting...");
+    process.exit(0);
+  })
+  .catch((error) => {
+    logging.errorLog("Something whent wrong, exiting on error:", error);
+    process.exit(1);
+  });
