3f50647c3889b8d46cbf925a6a117046e3babb8b
Gabe <gabrielgrechuk@outlook.com>
2025-04-29 01:23:11 -0300

    #4 - Implement main()

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
 import { parseCliArgs } from "./cli";
+import {
+  getPostgresDatabasesSchema,
+  instancePostgresDatabases,
+} from "./implementations/postgres";
+import { genFileText, snakeToPascal, writeFile } from "./tools";
+import * as logging from "./logging";
 
-function main() {
-  const args = parseCliArgs();
-  console.log(args);
+async function main() {
+  try {
+    const args = parseCliArgs();
+    const instances = instancePostgresDatabases({
+      databases: args.databases,
+      ssl: args.ssl,
+    });
+    const schemas = await getPostgresDatabasesSchema({
+      databases: instances,
+      tablesToIgnore: args.exclude_tables,
+    });
+
+    const writeInterfacesPromises = Object.keys(schemas).map((field) => {
+      return writeFile(
+        `${args.outdir ?? "."}/${snakeToPascal(field)}.interfaces.ts`,
+        genFileText(schemas[field]!),
+      );
+    });
+
+    await Promise.all(writeInterfacesPromises);
+  } catch (error) {
+    console.log("Error:", error);
+  }
 }
 
-main();
+await main().then(() => {
+  logging.infoLog("All done! Exiting...");
+  process.exit(0);
+});
